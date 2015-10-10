a9a8499d9e189870974b7d1352b4cbcf4661bcf7
Sergio <sergioforerop@hotmail.com>
2015-10-10 09:55:29 -0500

    MOD

--- cp-ejb/src/test/java/com/making/cp/negocio/EmisorServiceBeanTest.java
+++ cp-ejb/src/test/java/com/making/cp/negocio/EmisorServiceBeanTest.java
@@ -5,8 +5,6 @@
  */
 package com.making.cp.negocio;
 
-import static org.junit.Assert.*;
-
 /**
  *
  * @author Camilo Marroquin
@@ -16,13 +14,13 @@ public class EmisorServiceBeanTest {
     /**
      * Test of obtenerEmisores method, of class EmisorServiceBean.
      */
-    @org.junit.Test
-    public void testObtenerEmisores() {
-        System.out.println("obtenerEmisores");
-        EmisorServiceBean instance = new EmisorServiceBean();
-        instance.obtenerEmisores();
-        // TODO review the generated test code and remove the default call to fail.
-        assertTrue("The test case is a prototype.", true);
-    }
+//    @org.junit.Test
+//    public void testObtenerEmisores() {
+//        System.out.println("obtenerEmisores");
+//        EmisorServiceBean instance = new EmisorServiceBean();
+//        instance.obtenerEmisores();
+//        // TODO review the generated test code and remove the default call to fail.
+//        assertTrue("The test case is a prototype.", true);
+//    }
     
 }
