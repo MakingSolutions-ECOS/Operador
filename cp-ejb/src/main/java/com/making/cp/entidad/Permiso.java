3263ba55ff943a09d6857a8957f0cb4835193d76
angela <angela.suarez.torres@gmail.com>
2015-10-17 11:29:59 -0500

    MOD Entities

---
+++ cp-ejb/src/main/java/com/making/cp/entidad/Permiso.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.making.cp.entidad;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Your Name
+ */
+@Entity
+@Table(name = "PERMISO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Permiso.findAll", query = "SELECT p FROM Permiso p"),
+    @NamedQuery(name = "Permiso.findByCodigoPermiso", query = "SELECT p FROM Permiso p WHERE p.codigoPermiso = :codigoPermiso"),
+    @NamedQuery(name = "Permiso.findByCodigoGrupoUsuario", query = "SELECT p FROM Permiso p WHERE p.codigoGrupoUsuario = :codigoGrupoUsuario"),
+    @NamedQuery(name = "Permiso.findByNombrePermiso", query = "SELECT p FROM Permiso p WHERE p.nombrePermiso = :nombrePermiso")})
+public class Permiso implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CODIGO_PERMISO")
+    private Integer codigoPermiso;
+    @Column(name = "CODIGO_GRUPO_USUARIO")
+    private Integer codigoGrupoUsuario;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 15)
+    @Column(name = "NOMBRE_PERMISO")
+    private String nombrePermiso;
+    @OneToMany(mappedBy = "codigoPermiso")
+    private List<Grupo> grupoList;
+
+    public Permiso() {
+    }
+
+    public Permiso(Integer codigoPermiso) {
+        this.codigoPermiso = codigoPermiso;
+    }
+
+    public Permiso(Integer codigoPermiso, String nombrePermiso) {
+        this.codigoPermiso = codigoPermiso;
+        this.nombrePermiso = nombrePermiso;
+    }
+
+    public Integer getCodigoPermiso() {
+        return codigoPermiso;
+    }
+
+    public void setCodigoPermiso(Integer codigoPermiso) {
+        this.codigoPermiso = codigoPermiso;
+    }
+
+    public Integer getCodigoGrupoUsuario() {
+        return codigoGrupoUsuario;
+    }
+
+    public void setCodigoGrupoUsuario(Integer codigoGrupoUsuario) {
+        this.codigoGrupoUsuario = codigoGrupoUsuario;
+    }
+
+    public String getNombrePermiso() {
+        return nombrePermiso;
+    }
+
+    public void setNombrePermiso(String nombrePermiso) {
+        this.nombrePermiso = nombrePermiso;
+    }
+
+    @XmlTransient
+    public List<Grupo> getGrupoList() {
+        return grupoList;
+    }
+
+    public void setGrupoList(List<Grupo> grupoList) {
+        this.grupoList = grupoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codigoPermiso != null ? codigoPermiso.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Permiso)) {
+            return false;
+        }
+        Permiso other = (Permiso) object;
+        if ((this.codigoPermiso == null && other.codigoPermiso != null) || (this.codigoPermiso != null && !this.codigoPermiso.equals(other.codigoPermiso))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.making.cp.entidad.Permiso[ codigoPermiso=" + codigoPermiso + " ]";
+    }
+    
+}
---
+++ cp-ejb/src/main/java/com/making/cp/entidad/Publicidad.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.making.cp.entidad;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Your Name
+ */
+@Entity
+@Table(name = "PUBLICIDAD")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Publicidad.findAll", query = "SELECT p FROM Publicidad p"),
+    @NamedQuery(name = "Publicidad.findByCodigoPublicidad", query = "SELECT p FROM Publicidad p WHERE p.codigoPublicidad = :codigoPublicidad"),
+    @NamedQuery(name = "Publicidad.findByFechaCreacionPublicidad", query = "SELECT p FROM Publicidad p WHERE p.fechaCreacionPublicidad = :fechaCreacionPublicidad"),
+    @NamedQuery(name = "Publicidad.findByFechaInicioPublicacion", query = "SELECT p FROM Publicidad p WHERE p.fechaInicioPublicacion = :fechaInicioPublicacion"),
+    @NamedQuery(name = "Publicidad.findByDuracionPublicacionMinutos", query = "SELECT p FROM Publicidad p WHERE p.duracionPublicacionMinutos = :duracionPublicacionMinutos"),
+    @NamedQuery(name = "Publicidad.findByHabilitada", query = "SELECT p FROM Publicidad p WHERE p.habilitada = :habilitada")})
+public class Publicidad implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CODIGO_PUBLICIDAD")
+    private Integer codigoPublicidad;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "FECHA_CREACION_PUBLICIDAD")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaCreacionPublicidad;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "FECHA_INICIO_PUBLICACION")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaInicioPublicacion;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "DURACION_PUBLICACION_MINUTOS")
+    private int duracionPublicacionMinutos;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "HABILITADA")
+    private short habilitada;
+    @OneToMany(mappedBy = "codigoPublicidad")
+    private List<PublicidadImagen> publicidadImagenList;
+    @JoinColumn(name = "CODIGO_CIUDADANO", referencedColumnName = "CODIGO_CIUDADANO")
+    @ManyToOne
+    private Ciudadano codigoCiudadano;
+
+    public Publicidad() {
+    }
+
+    public Publicidad(Integer codigoPublicidad) {
+        this.codigoPublicidad = codigoPublicidad;
+    }
+
+    public Publicidad(Integer codigoPublicidad, Date fechaCreacionPublicidad, Date fechaInicioPublicacion, int duracionPublicacionMinutos, short habilitada) {
+        this.codigoPublicidad = codigoPublicidad;
+        this.fechaCreacionPublicidad = fechaCreacionPublicidad;
+        this.fechaInicioPublicacion = fechaInicioPublicacion;
+        this.duracionPublicacionMinutos = duracionPublicacionMinutos;
+        this.habilitada = habilitada;
+    }
+
+    public Integer getCodigoPublicidad() {
+        return codigoPublicidad;
+    }
+
+    public void setCodigoPublicidad(Integer codigoPublicidad) {
+        this.codigoPublicidad = codigoPublicidad;
+    }
+
+    public Date getFechaCreacionPublicidad() {
+        return fechaCreacionPublicidad;
+    }
+
+    public void setFechaCreacionPublicidad(Date fechaCreacionPublicidad) {
+        this.fechaCreacionPublicidad = fechaCreacionPublicidad;
+    }
+
+    public Date getFechaInicioPublicacion() {
+        return fechaInicioPublicacion;
+    }
+
+    public void setFechaInicioPublicacion(Date fechaInicioPublicacion) {
+        this.fechaInicioPublicacion = fechaInicioPublicacion;
+    }
+
+    public int getDuracionPublicacionMinutos() {
+        return duracionPublicacionMinutos;
+    }
+
+    public void setDuracionPublicacionMinutos(int duracionPublicacionMinutos) {
+        this.duracionPublicacionMinutos = duracionPublicacionMinutos;
+    }
+
+    public short getHabilitada() {
+        return habilitada;
+    }
+
+    public void setHabilitada(short habilitada) {
+        this.habilitada = habilitada;
+    }
+
+    @XmlTransient
+    public List<PublicidadImagen> getPublicidadImagenList() {
+        return publicidadImagenList;
+    }
+
+    public void setPublicidadImagenList(List<PublicidadImagen> publicidadImagenList) {
+        this.publicidadImagenList = publicidadImagenList;
+    }
+
+    public Ciudadano getCodigoCiudadano() {
+        return codigoCiudadano;
+    }
+
+    public void setCodigoCiudadano(Ciudadano codigoCiudadano) {
+        this.codigoCiudadano = codigoCiudadano;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codigoPublicidad != null ? codigoPublicidad.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Publicidad)) {
+            return false;
+        }
+        Publicidad other = (Publicidad) object;
+        if ((this.codigoPublicidad == null && other.codigoPublicidad != null) || (this.codigoPublicidad != null && !this.codigoPublicidad.equals(other.codigoPublicidad))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.making.cp.entidad.Publicidad[ codigoPublicidad=" + codigoPublicidad + " ]";
+    }
+    
+}
