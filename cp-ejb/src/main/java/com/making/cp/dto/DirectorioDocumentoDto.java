afad67b84a6c69a364c321ad814315608cb6842e
John Dany Osorio R <johnoso27@hotmail.com>
2015-11-18 19:39:25 -0500

    ADD
    Se agrega dto para mapear el entity DirectorioDocumento

---
+++ cp-ejb/src/main/java/com/making/cp/dto/DirectorioDocumentoDto.java
@@ -0,0 +1,59 @@
+package com.making.cp.dto;
+
+import java.util.List;
+
+/**
+ * Clase que representa el entity DirectorioDocumento
+ *
+ * @author John Dany
+ */
+public class DirectorioDocumentoDto {
+
+    private Integer codigoDirectorioDocumento;
+    private List<DirectorioDto> directorioList;
+    private List<DocumentoDto> documentoList;
+
+    /**
+     * Incializa una nueva instancia de DirectorioDocumentoDto
+     */
+    public DirectorioDocumentoDto() {
+    }
+
+    /**
+     * Inicializa una nueva instancia de DirectorioDocumentoDto con parámetros.
+     *
+     * @param codigoDirectorioDocumento
+     * @param directorioList
+     * @param documentoList
+     */
+    public DirectorioDocumentoDto(Integer codigoDirectorioDocumento, List<DirectorioDto> directorioList, List<DocumentoDto> documentoList) {
+        this.codigoDirectorioDocumento = codigoDirectorioDocumento;
+        this.directorioList = directorioList;
+        this.documentoList = documentoList;
+    }
+
+    public Integer getCodigoDirectorioDocumento() {
+        return codigoDirectorioDocumento;
+    }
+
+    public void setCodigoDirectorioDocumento(Integer codigoDirectorioDocumento) {
+        this.codigoDirectorioDocumento = codigoDirectorioDocumento;
+    }
+
+    public List<DirectorioDto> getDirectorioList() {
+        return directorioList;
+    }
+
+    public void setDirectorioList(List<DirectorioDto> directorioList) {
+        this.directorioList = directorioList;
+    }
+
+    public List<DocumentoDto> getDocumentoList() {
+        return documentoList;
+    }
+
+    public void setDocumentoList(List<DocumentoDto> documentoList) {
+        this.documentoList = documentoList;
+    }
+
+}
