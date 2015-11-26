c3351fc4651497ca8c041d7182a451db36e86146
angela <angela.suarez.torres@gmail.com>
2015-11-26 14:48:42 -0500

    MOD  (angelaEST) Modificaciones trámite

--- cp-ejb/src/main/java/com/making/cp/negocio/Helper/ConstantesOperador.java
+++ cp-ejb/src/main/java/com/making/cp/negocio/Helper/ConstantesOperador.java
@@ -10,6 +10,7 @@ package com.making.cp.negocio.Helper;
  * @author Sergio Forero
  */
 public class ConstantesOperador {
+    public static final Integer ESTADO_TRAMITE_PENDIENTE_RESPUESTA=1;
     public static final Integer ESTADO_TRAMITE_FINALIZADO=3;
     public static final Integer ESTADO_TRAMITE_NOTIFICADO=4;
     
